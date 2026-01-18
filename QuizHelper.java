6470b8d69a7e2d4f06b0df27167496128f0b8712
indureddy246 <146846902+indureddy246@users.noreply.github.com>
2026-01-18 19:52:33 +0530

    Add QuizHelper class with message methods

---
+++ QuizHelper.java
@@ -0,0 +1,13 @@
+
+package gqtindu;
+
+public class QuizHelper {
+    public static void wrongEnd(String name, int reward) {
+        System.out.println(Colors.RED + name + " it is wrong answer. Game Over." + Colors.RESET);
+        System.out.println("Final Reward: " + reward);
+    }
+
+    public static void correctMsg(String name, int reward) {
+        System.out.println(Colors.GREEN + name + " it is correct answer. You rewarded with " + reward + Colors.RESET);
+    }
+}
